f726d28daa16174071da73e72c152a404cdc5e55
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-12-07 23:39:36 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,14 @@
+//建構應用程式伺服器
+//引用 Express 與 Express 路由器
+const Express = require('express')
+const app = Express()
+
+//引用路由模組
+app.get('/', (req, res) => {
+  res.send("hello world!")
+})
+//設定 port 3000
+//啟動伺服器並監聽
+app.listen(3000, () => {
+  console.log(`App is running on http://localhost:3000`)
+})
